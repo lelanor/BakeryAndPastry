0754e7b6900fe42f5cdd03f3c7b245eb4508b5b5
Enrico Lo Faro <elofaro@outlook.fr>
2021-09-13 16:27:01 +0200

    :recycle: all web pages are dynamic and connected to database

--- src/main/java/com/elofaro/bakeryandpastry/controller/LandingController.java
+++ src/main/java/com/elofaro/bakeryandpastry/controller/LandingController.java
@@ -1,16 +1,36 @@
 package com.elofaro.bakeryandpastry.controller;
 
+import com.elofaro.bakeryandpastry.dao.ProductDAO;
+import com.elofaro.bakeryandpastry.model.Product;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 @RequestMapping("/")
 public class LandingController {
 
+    ProductDAO dao;
+
+    public LandingController(@Autowired ProductDAO dao) {
+        this.dao = dao;
+    }
+
     @GetMapping
-    public String displayIndex(Model model){
+    public String displayIndex(Model model) {
+
+        List<Product> productList = new ArrayList<>();
+        for (Product product : dao.findAll()) {
+            productList.add(product);
+        }
+
+        model.addAttribute("products", productList);
+
         return "landing";
     }
 }
